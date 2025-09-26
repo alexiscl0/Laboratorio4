6c1564bf078a36bb6d948865685fc288eb297d8e
Alexis <acalcinala@unsa.edu.pe>
2025-09-26 18:05:49 -0500

    aq

--- Libro.java
+++ Libro.java
@@ -1,8 +1,20 @@
+import java.util.*;
+
 public class Libro {
     private String Titulo;
     private String Autor;
     private int ISBM;
     private boolean disponible;
+    private Scanner sc;
+
+    public Libro() {
+        System.out.println("Ingrese titulo del libro: ");
+        setTitulo(sc.nextLine());
+        System.out.println("Ingresar autor del libro: ");
+        setAutor(sc.nextLine());
+        System.out.println("Ingresar ISBM(4 cifras)");
+        setISBM(sc.nextInt());
+    }
 
     public void setTitulo(String Titulo) {
         this.Titulo = Titulo;
@@ -17,7 +29,15 @@ public class Libro {
     }
 
     public void setISBM(int ISBM) {
-        this.ISBM = ISBM;
+        boolean correcto;
+        do {
+            correcto = false;
+            if (ISBM > 999) {
+                this.ISBM = ISBM;
+                correcto = true;
+                break;
+            }
+        } while (correcto == false);
     }
 
     public String getTitulo() {
@@ -34,5 +54,13 @@ public class Libro {
 
     public int getISBM() {
         return this.ISBM;
+    }
+
+    @Override
+    public String toString() {
+        return "Titulo\t" + getTitulo() + "\n"
+                + "Autor\t" + getAutor() + "\n"
+                + "ISBM\t" + getISBM() + "\n" +
+                "Disponible\t" + getDisponible();
     }
 }
